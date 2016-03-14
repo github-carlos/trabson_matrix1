8f753bbe5cc6ff7edcddd505fb1f2ff69d814a45
zevet <carloseduardo057@gmail.com>
2016-03-14 20:20:14 -0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -2,26 +2,46 @@
 #define MATRIZ_H
 
 
-class Matriz
+class Matrix
 {
 private:
     int rows;
     int columns;
     int **vet_matrix;
 
-    bool equal_dimensions( Matriz b) {return rows == b.getRows() && columns == b.getColumns();}
+    bool columnsA_Equal_LinesB(Matrix &B);
+    bool square() {return columns == rows;}
+    bool equal_dimensions( Matrix b) {return rows == b.getRows() && columns == b.getColumns();}
+
+
 public:
-    Matriz(const int rows, const int columns);
+    Matrix(const int rows, const int columns);
+
     int getColumns() { return columns; }
     int getRows() const { return rows; }
     int getElement(int &x, int&y) {return vet_matrix[x][y];}
+
     void set(int x, int y, int value);
-    Matriz transposed();
-    void fill();
-
-    Matriz operator +(Matriz b);
-
     void show();
+    Matrix transposed();
+    
+
+    Matrix operator +(Matrix b);
+    Matrix operator-(Matrix b);
+    Matrix operator*(Matrix b);
+    Matrix operator*(int);
+    Matrix potencia(Matrix &b);
+
+    bool simetrica(Matrix &b);
+    bool identidade(Matrix &b);
+    bool igual(Matrix &b);
+    bool diferente(Matrix &b);
+    bool ortogonal();
+    bool permutacao(Matrix &b);
+    bool upperTriangular();
+    bool lowerTriangular(Matrix &b);
+
+    
 };
 
 #endif // MATRIZ_H
