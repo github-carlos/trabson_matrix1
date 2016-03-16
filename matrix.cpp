9acf7dc1deb1167d6084effdfb2396cc7697ab15
buck999 <thiagofreitas999@gmail.com>
2016-03-15 21:26:31 -0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,4 @@
-#include "matriz.h"
+#include "matrix.h"
 #include <iostream>
 using namespace std;
 
@@ -7,11 +7,10 @@ Matrix::Matrix(const int rows , const int columns )
 
     this->rows = rows;
     this->columns = columns;
-    vet_matrix = new int*[this->columns];//Columns allocation
+    vet_matrix = new int*[this->rows];//Columns allocation
 
-
-    for(int i = 0; i < this->columns; i++)
-        vet_matrix[i] = new int[this->rows]; //Rows allocation
+    for(int i = 0; i < this->rows; i++)
+        vet_matrix[i] = new int[this->columns]; //Rows allocation
 }
 void Matrix::set(int x, int y, int value) //
 {
@@ -23,8 +22,12 @@ Matrix Matrix::transposed()
     Matrix aux(columns, rows);
 
     for(int i = 0; i < columns; i++)
+    {
         for(int j = 0; j < rows; j++)
-            aux.set(j,i,vet_matrix[i][j]);
+        {
+            aux.set(i,j,vet_matrix[j][i]);
+        }
+    }
 
     return aux;
 }
@@ -36,7 +39,7 @@ void Matrix::fill()
         for(int j = 0; j < columns; j++){
             cout << "[" << i << "] " << "[" << j << "] = ";
             cin >> k;
-            vet_matrix[j][i] = k;
+            vet_matrix[i][j] = k;
         }
     }
 }
@@ -61,3 +64,71 @@ Matrix Matrix::operator +(Matrix b)
 
 }
 
+Matrix Matrix::operator -(Matrix b)
+{
+    if(equal_dimensions(b))
+    {
+        Matrix c(rows, columns);
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < columns;j++)
+            {
+                c.set(i, j, vet_matrix[i][j] - b.getElement(i, j));
+            }
+        }
+        return c;
+    }
+    throw  std::string("\nNao eh possivel fazer a subtracao.\n");
+}
+
+Matrix Matrix::operator *(Matrix b)
+{
+    if(columnsA_Equal_LinesB(b))
+    {
+        Matrix c(rows, b.getColumns());
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; i < b.getColumns(); j++)
+            {
+//                c.set(i, j, vet_matrix[i][j]=0);
+
+                for(int k = 0; k < columns; k++)
+                {
+                    c.set(i, j, c.getElement(i,j) + vet_matrix[i][k] * b.getElement(k,j));
+                }
+            }
+        }
+        return c;
+    }
+    throw std::string("\nNao eh possivel fazer a multiplicacao.\n");
+}
+
+bool Matrix::upperTriangular()
+{
+    if(square()==true)
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(vet_matrix[i][j] != 0)
+                    return false;
+                return true;
+            }
+        }
+}
+
+bool Matrix::lowerTriangular()
+{
+    if(square()==true)
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = i + 1; j < rows; j++)
+            {
+                if(vet_matrix[i][j] != 0)
+                    return false;
+                return true;
+            }
+        }
+}
