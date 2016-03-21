4f80bd83e47b66017d3782892f8db47116f78c33
buck999 <thiagofreitas999@gmail.com>
2016-03-21 17:57:10 -0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -7,46 +7,32 @@ class Matrix
 private:
     int rows;
     int columns;
-    int **vet_matrix;
-
+    int **_matrix;
     bool columnsA_Equal_LinesB(Matrix b){return columns == b.getRows();}
     bool square() {return columns == rows;}
     bool equal_dimensions( Matrix b) {return rows == b.getRows() && columns == b.getColumns();}
-
-
 public:
     Matrix(const int rows, const int columns);
-
     int getColumns() const { return columns; }
     int getRows() const { return rows; }
-    int getElement(int &x, int&y) {return vet_matrix[x][y];}
-
+    int getElement(int &x, int&y) {return _matrix[x][y];}
     void set(int x, int y, int value);
     void fill();
-
     Matrix transposed();
     Matrix operator +(Matrix b);
-    Matrix operator-(Matrix b);
-    Matrix operator*(Matrix b);
-    Matrix operator*(int);
+    Matrix operator -(Matrix b);
+    Matrix operator *(Matrix b);
+    Matrix operator *(int);
     bool operator ==(Matrix &b);
     bool operator !=(Matrix &b);
     bool upperTriangular();
     bool lowerTriangular();
-    bool igual(Matrix &b);
-    bool identidade();
-    bool ortogonal();
-    bool permutacao();
-    static bool simetrica(Matrix a);
-
-
-    int potencia(int x);
-
-
-
-
-
-
+    bool equal(Matrix &b);
+    bool identity();
+    bool orthogonal();
+    bool permutation();
+    static bool symmetric(Matrix a);
+    int power(int x);
 };
 
 #endif // MATRIZ_H
